8a93a21e979a3062410166aba1019bf1050a60d0
dam1 <dam1@DAM13>
2024-02-02 12:01:29 +0100

    A la 2a branch

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("1a modificació Git");
         System.out.println("2a modificació GitHub");
         System.out.println("3a modificació Git");
+        System.out.println("4a modificació Git Branca2");
     }
 }
